5eec0f192ab5a66fda7e81e17b0034f4def3aea1
steve.zuo <steve.zuo@outlook.com>
2013-05-21 14:47:25 +0800

    add logs

--- system_manager/main.cpp
+++ system_manager/main.cpp
@@ -2,6 +2,8 @@
 
 #include <QtGui/QApplication>
 #include <QString>
+#include <QtGui/qscreen_qws.h>
+#include <QtGui/qwsdisplay_qws.h>
 
 #include "System/inc/system_manager.h"
 
@@ -12,5 +14,20 @@ int main(int argc, char* argv[])
     // Otherwise, tslib can not initialize the touch screen.
     QApplication app(argc, argv);
     //SystemManagerAdaptor adaptor(app);
+
+    QScreen *instance = QScreen::instance();
+    const uchar *data = instance->base();
+    qDebug("instance->base:%p", data);
+    int degree = 0;
+    degree = instance->transformOrientation() * 90;
+    int width = instance->width();
+    int height = instance->height();
+    if (degree == 90 || degree == 270)
+    {
+        std::swap(width, height);
+    }
+    qDebug("width:%d, height:%d, pixelFormat:%d", width, height, instance->pixelFormat());
+    QImage image(data, width, height, instance->pixelFormat());
+
     return app.exec();
 }
